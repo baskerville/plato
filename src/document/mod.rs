25f61dfa69453fb97e2020367f665dbfe1092e18
Bastien Dejean <nihilhill@gmail.com>
2017-08-02 18:52:21 +0200

    Add Document abstraction

--- src/document/mod.rs
+++ src/document/mod.rs
@@ -1,8 +1,12 @@
-use std::collections::HashMap;
-use geom::Rectangle;
-
 pub mod djvu;
 pub mod pdf;
+
+use std::cmp;
+use std::path::Path;
+use geom::Rectangle;
+use document::djvu::{DjvuOpener};
+use document::pdf::{PdfOpener};
+use framebuffer::Bitmap;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum LayerGrain {
@@ -28,4 +32,76 @@ pub struct TocEntry {
     title: String,
     page: usize,
     children: Vec<TocEntry>,
+}
+
+pub trait Document {
+    fn pages_count(&self) -> usize;
+    fn toc(&self) -> Option<Vec<TocEntry>>;
+    fn text(&self, index: usize) -> Option<TextLayer>;
+    fn title(&self) -> Option<String>;
+    fn author(&self) -> Option<String>;
+    // fn dims(&self, index: usize) -> Option<(f32, f32)>;
+    // fn render(&self, index: usize, scale: f32) -> Option<Bitmap>;
+    fn is_reflowable(&self) -> bool;
+    // fn page(&self, index: usize) -> Option<Box<Page>>;
+}
+
+pub trait Page {
+    fn dims(&self) -> (u32, u32);
+    fn render(&self, scale: f32) -> Option<Bitmap>;
+}
+
+pub fn file_kind<P: AsRef<Path>>(path: P) -> Option<String> {
+    path.as_ref().extension()
+        .and_then(|os_ext| os_ext.to_str())
+        .map(|ext| ext.to_lowercase())
+}
+
+pub trait HumanSize {
+    fn human_size(&self) -> String;
+}
+
+impl HumanSize for u64 {
+    fn human_size(&self) -> String {
+        let value = *self as f32;
+        let level = cmp::min(3, value.log(1024f32).floor() as usize);
+        let factor = value / (1024f32).powi(level as i32);
+        let precision = level.saturating_sub(1 + factor.log(10.0).floor() as usize);
+        format!("{0:.1$} {2}", factor, precision, ['B', 'K', 'M', 'G'][level])
+    }
+}
+
+impl TextLayer {
+    pub fn words(&self) -> Vec<String> {
+        match self.grain {
+            LayerGrain::Word => {
+                vec![self.text.as_ref().unwrap().to_string()]
+            },
+            LayerGrain::Character => vec![],
+            _ => {
+                let mut result = Vec::new();
+                for child in &self.children {
+                    result.extend_from_slice(&child.words());
+                }
+                result
+            }
+        }
+    }
+}
+
+pub fn open<P: AsRef<Path>>(path: P) -> Option<Box<Document>> {
+    file_kind(path.as_ref()).and_then(|k| {
+        match k.as_ref() {
+            "djvu" | "djv" => {
+                DjvuOpener::new()
+                    .and_then(|o| o.open(path)
+                                   .map(|d| Box::new(d) as Box<Document>))
+            },
+            _ => {
+                PdfOpener::new()
+                    .and_then(|o| o.open(path)
+                                   .map(|d| Box::new(d) as Box<Document>))
+            },
+        }
+    })
 }
