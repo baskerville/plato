abb271df6095d406f737daac3fe23ffc76fa41e0
Bastien Dejean <nihilhill@gmail.com>
2017-08-02 18:49:33 +0200

    Add lcms2 dependency

--- build.rs
+++ build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("cargo:rustc-flags=-L libs -l harfbuzz -l freetype -l jpeg -l openjp2 -l jbig2dec -l bz2 -l z -l m");
+    println!("cargo:rustc-flags=-L libs -l jpeg -l openjp2 -l jbig2dec -l lcms2 -l bz2 -l z -l m");
 }
