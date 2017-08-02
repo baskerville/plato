dfd44bf0a9414e08ac5e0ed783468209c772406b
Bastien Dejean <nihilhill@gmail.com>
2017-08-02 18:51:27 +0200

    Add scale_by_dpi

--- src/unit.rs
+++ src/unit.rs
@@ -1,6 +1,13 @@
 const MILLIMETERS_PER_INCH: f32 = 25.4;
+const POINTS_PER_INCH: f32 = 72.0;
+const BASE_DPI: f32 = 300.0;
 
 #[inline]
 pub fn mm_to_in(mm: f32) -> f32 {
     mm / MILLIMETERS_PER_INCH
+}
+
+#[inline]
+pub fn scale_by_dpi(x: f32, dpi: u16) -> f32 {
+    x * (dpi as f32) / BASE_DPI
 }
