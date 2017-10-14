3193b7c7076ef8cd09881dff9b324e5dab7144fc
Bastien Dejean <nihilhill@gmail.com>
2017-10-14 14:23:19 +0200

    Add Settings::load

--- src/settings.rs
+++ src/settings.rs
@@ -1,14 +1,27 @@
+extern crate serde_json;
+
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use errors::*;
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(default, rename_all = "camelCase")]
 pub struct Settings {
-    library_path: String,
-    refresh_every: Option<u8>,
+    pub library_path: PathBuf,
+    pub refresh_every: Option<u8>,
+}
+
+impl Settings {
+    pub fn load<P: AsRef<Path>>(path: P) -> Result<Settings> {
+        let file = File::open(path).chain_err(|| "Can't open settings file")?;
+        serde_json::from_reader(file).chain_err(|| "Can't parse settings file")
+    }
 }
 
 impl Default for Settings {
     fn default() -> Self {
         Settings {
-            library_path: "/mnt/onboard/books".to_string(),
+            library_path: PathBuf::from("/mnt/onboard/books"),
             refresh_every: Some(24),
         }
     }
