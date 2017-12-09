d58ec56452334da5df914dcb6ea2b58299e92c0d
Bastien Dejean <nihilhill@gmail.com>
2017-12-09 18:25:19 +0100

    Get rid of the lcms2 dependency

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("cargo:rustc-flags=-L libs -l jpeg -l openjp2 -l jbig2dec -l lcms2 -l bz2 -l z -l m");
+    println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
+    println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
 }
