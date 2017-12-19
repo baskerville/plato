ca46b6c3a099157d5a3cf01eb45a32f4ae2dcb36
Bastien Dejean <nihilhill@gmail.com>
2017-12-19 19:27:44 +0100

    Add spacing

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 sync
+
 echo 1 > /sys/power/state-extended
 [ "$PRODUCT" == phoenix ] && sleep 2
 echo mem > /sys/power/state
--- scripts/wifi_disable.sh
+++ scripts/wifi_disable.sh
@@ -4,6 +4,7 @@ lsmod | grep -q sdio_wifi_pwr || exit 1
 
 wlarm_le -i eth0 down
 ifconfig eth0 down
+
 rmmod -r "$WIFI_MODULE"
 rmmod -r sdio_wifi_pwr
 
