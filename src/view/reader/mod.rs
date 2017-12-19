baskerville/plato

commit feadc26748b4ae01c59f688414dd274094279839

Parse USB plug/unplug events

src/view/reader/mod.rs
@@ -449,7 +449,7 @@ impl View for Reader {
                 self.toggle_margin_cropper(false, hub, &mut context.fonts);
                 true
             },
-            Event::Select(EntryId::Quit) | Event::Back => {
+            Event::Select(EntryId::Reboot) | Event::Select(EntryId::StartNickel) | Event::Back => {
                 self.quit(context);
                 false
             },

src/view/mod.rs
@@ -33,7 +33,7 @@ use downcast_rs::Downcast;
 use font::Fonts;
 use metadata::{Info, SortMethod, Margin};
 use framebuffer::{Framebuffer, UpdateMode};
-use input::FingerStatus;
+use input::{DeviceEvent, FingerStatus};
 use gesture::GestureEvent;
 use view::key::KeyKind;
 use app::Context;
@@ -177,6 +177,7 @@ pub enum Event {
     Render(Rectangle, UpdateMode),
     RenderNoWait(Rectangle, UpdateMode),
     Expose(Rectangle),
+    Device(DeviceEvent),
     Gesture(GestureEvent),
     Keyboard(KeyboardEvent),
     Key(KeyKind),
@@ -287,6 +288,10 @@ pub enum EntryId {
     ToggleInverted,
     ToggleMonochrome,
     TakeScreenshot,
+    Suspend,
+    PowerOff,
+    Reboot,
+    StartNickel,
     Quit,
 }
 

src/gesture.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 use std::thread;
 use unit::mm_to_in;
 use input::{DeviceEvent, FingerStatus, ButtonCode, ButtonStatus};
+use view::Event;
 use device::CURRENT_DEVICE;
 use geom::{Point, Dir, Axis};
 
@@ -39,32 +40,8 @@ pub enum GestureEvent {
         quarter_turns: i8,
         center: Point,
     },
-    Finger {
-        id: i32,
-        time: f64,
-        status: FingerStatus,
-        position: Point,
-    },
-    Button {
-        time: f64,
-        code: ButtonCode,
-        status: ButtonStatus,
-    },
     HoldFinger(Point),
     HoldButton(ButtonCode),
-}
-
-impl GestureEvent {
-    pub fn from_device_event(evt: DeviceEvent) -> GestureEvent {
-        match evt {
-            DeviceEvent::Finger { id, time, status, position } => {
-                GestureEvent::Finger { id, time, status, position }
-            },
-            DeviceEvent::Button { time, code, status } => {
-                GestureEvent::Button { time, code, status }
-            }
-        }
-    }
 }
 
 #[derive(Debug)]
@@ -74,19 +51,19 @@ pub struct TouchState {
     current: Point,
 }
 
-pub fn gesture_events(rx: Receiver<DeviceEvent>) -> Receiver<GestureEvent> {
+pub fn gesture_events(rx: Receiver<DeviceEvent>) -> Receiver<Event> {
     let (ty, ry) = mpsc::channel();
     thread::spawn(move || parse_gesture_events(&rx, &ty));
     ry
 }
 
-pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<GestureEvent>) {
+pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<Event>) {
     let contacts: Arc<Mutex<FnvHashMap<i32, TouchState>>> = Arc::new(Mutex::new(FnvHashMap::default()));
     let buttons: Arc<Mutex<FnvHashMap<ButtonCode, f64>>> = Arc::new(Mutex::new(FnvHashMap::default()));
     let mut segments: Vec<(Point, Point)> = Vec::new();
     let jitter = CURRENT_DEVICE.dpi as f32 * mm_to_in(JITTER_TOLERANCE_MM);
     while let Ok(evt) = rx.recv() {
-        ty.send(GestureEvent::from_device_event(evt)).unwrap();
+        ty.send(Event::Device(evt)).unwrap();
         match evt {
             DeviceEvent::Finger { status: FingerStatus::Down, position, id, time } => {
                 let mut ct = contacts.lock().unwrap();
@@ -99,7 +76,7 @@ pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<GestureEvent
                     let mut will_remove = None;
                     if let Some(ts) = ct.get(&id) {
                         if ts.time == time && (ts.current - position).length() < jitter {
-                            ty.send(GestureEvent::HoldFinger(position)).unwrap();
+                            ty.send(Event::Gesture(GestureEvent::HoldFinger(position))).unwrap();
                             will_remove = Some(id);
                         }
                     }
@@ -123,53 +100,53 @@ pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<GestureEvent
                     let len = segments.len();
                     if len == 1 {
                         let ge = interpret_segment(segments.pop().unwrap(), jitter);
-                        ty.send(ge).unwrap();
+                        ty.send(Event::Gesture(ge)).unwrap();
                     } else if len == 2 {
                         let ge1 = interpret_segment(segments.pop().unwrap(), jitter);
                         let ge2 = interpret_segment(segments.pop().unwrap(), jitter);
                         match (ge1, ge2) {
                             (GestureEvent::Tap { center: c1, .. }, GestureEvent::Tap { center: c2, .. }) => {
-                                ty.send(GestureEvent::Tap {
+                                ty.send(Event::Gesture(GestureEvent::Tap {
                                     center: (c1 + c2) / 2,
                                     fingers_count: 2,
-                                }).unwrap();
+                                })).unwrap();
                             }
                             (GestureEvent::Swipe { dir: d1, start: s1, end: e1, .. },
                              GestureEvent::Swipe { dir: d2, start: s2, end: e2, .. }) if d1 == d2 => {
-                                ty.send(GestureEvent::Swipe {
+                                ty.send(Event::Gesture(GestureEvent::Swipe {
                                     dir: d1,
                                     start: (s1 + s2) / 2,
                                     end: (e1 + e2) / 2,
                                     fingers_count: 2,
-                                }).unwrap();
+                                })).unwrap();
                             },
                             (GestureEvent::Swipe { dir: d1, start: s1, end: e1, .. },
                              GestureEvent::Swipe { dir: d2, start: s2, end: e2, .. }) if d1 == d2.opposite() => {
                                 let ds = (s2 - s1).length();
                                 let de = (e2 - e1).length();
                                 if ds > de {
-                                    ty.send(GestureEvent::Pinch {
+                                    ty.send(Event::Gesture(GestureEvent::Pinch {
                                         axis: d1.axis(),
                                         target: (e1 + e2) / 2,
                                         strength: (ds - de) as u32,
-                                    }).unwrap();
+                                    })).unwrap();
                                 } else {
-                                    ty.send(GestureEvent::Spread {
+                                    ty.send(Event::Gesture(GestureEvent::Spread {
                                         axis: d1.axis(),
                                         target: (s1 + s2) / 2,
                                         strength: (de - ds) as u32,
-                                    }).unwrap();
+                                    })).unwrap();
                                 }
                             },
                             (GestureEvent::Swipe { start: s, end: e, .. }, GestureEvent::Tap { center: c, .. }) | 
                             (GestureEvent::Tap { center: c, .. }, GestureEvent::Swipe { start: s, end: e, .. }) => {
                                 let angle = ((s - c).angle() - (e - c).angle()).to_degrees();
                                 let quarter_turns = (angle.signum() * (angle / 90.0).abs().ceil()) as i8;
-                                ty.send(GestureEvent::Rotate {
+                                ty.send(Event::Gesture(GestureEvent::Rotate {
                                     angle: angle,
                                     quarter_turns: quarter_turns,
                                     center: c,
-                                }).unwrap();
+                                })).unwrap();
                             },
                             _ => (),
                         }
@@ -188,7 +165,7 @@ pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<GestureEvent
                     let bt = buttons.lock().unwrap();
                     if let Some(&initial_time) = bt.get(&code) {
                         if initial_time == time {
-                            ty.send(GestureEvent::HoldButton(code)).unwrap();
+                            ty.send(Event::Gesture(GestureEvent::HoldButton(code))).unwrap();
                         }
                     }
                 });
@@ -197,6 +174,7 @@ pub fn parse_gesture_events(rx: &Receiver<DeviceEvent>, ty: &Sender<GestureEvent
                 let mut bt = buttons.lock().unwrap();
                 bt.remove(&code);
             },
+            _ => (),
         }
     }
 }

src/input.rs
@@ -28,8 +28,10 @@ pub const ABS_MT_POSITION_X: u16 = 53;
 pub const ABS_MT_POSITION_Y: u16 = 54;
 pub const ABS_X: u16 = 0;
 pub const ABS_Y: u16 = 1;
+
 pub const KEY_POWER: u16 = 116;
 pub const KEY_HOME: u16 = 102;
+pub const SLEEP_COVER: u16 = 59;
 
 pub const SINGLE_TOUCH_CODES: TouchCodes = TouchCodes {
     report: SYN_REPORT,
@@ -85,7 +87,7 @@ pub enum ButtonStatus {
 pub enum ButtonCode {
     Power,
     Home,
-    Unknown,
+    Raw(u16),
 }
 
 impl ButtonCode {
@@ -95,7 +97,7 @@ impl ButtonCode {
         } else if code == KEY_HOME {
             ButtonCode::Home
         } else {
-            ButtonCode::Unknown
+            ButtonCode::Raw(code)
         }
     }
 }
@@ -113,6 +115,10 @@ pub enum DeviceEvent {
         code: ButtonCode,
         status: ButtonStatus,
     },
+    Plug,
+    Unplug,
+    CoverOn,
+    CoverOff,
 }
 
 pub fn seconds(time: libc::timeval) -> f64 {
@@ -161,6 +167,43 @@ pub fn parse_raw_events(paths: &[String], tx: &Sender<InputEvent>) -> Result<()>
     }
 
     Ok(())
+}
+
+pub fn usb_events() -> Receiver<DeviceEvent> {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || parse_usb_events(&tx));
+    rx
+}
+
+fn parse_usb_events(tx: &Sender<DeviceEvent>) {
+    let mut file = File::open("/tmp/nickel-hardware-status").unwrap();
+    let fd = file.as_raw_fd();
+
+    let mut pfds = [libc::pollfd { fd: fd, events: libc::POLLIN, revents: 0 }];
+
+    loop {
+        let ret = unsafe { libc::poll(pfds.as_mut_ptr(), pfds.len() as libc::nfds_t, -1) };
+
+        if ret < 0 {
+            break;
+        }
+
+        for pfd in pfds.iter() {
+            if pfd.revents & libc::POLLIN != 0 {
+                let mut buf = String::new();
+                if file.read_to_string(&mut buf).is_err() {
+                    break;
+                }
+
+                let msg = buf.trim_right();
+                if msg == "usb plug add" {
+                    tx.send(DeviceEvent::Plug).unwrap();
+                } else if msg == "usb plug remove" {
+                    tx.send(DeviceEvent::Unplug).unwrap();
+                }
+            }
+        }
+    }
 }
 
 pub fn device_events(rx: Receiver<InputEvent>, dims: (u32, u32)) -> Receiver<DeviceEvent> {
@@ -223,12 +266,20 @@ pub fn parse_device_events(rx: &Receiver<InputEvent>, ty: &Sender<DeviceEvent>, 
                 }
             }
         } else if evt.kind == EV_KEY {
-            ty.send(DeviceEvent::Button {
-                time: seconds(evt.time),
-                code: ButtonCode::from_raw(evt.code),
-                status: if evt.value == 1 { ButtonStatus::Pressed } else
-                                          { ButtonStatus::Released },
-            }).unwrap();
+            if evt.code == SLEEP_COVER {
+                if evt.value == 1 {
+                    ty.send(DeviceEvent::CoverOn).unwrap();
+                } else {
+                    ty.send(DeviceEvent::CoverOff).unwrap();
+                }
+            } else {
+                ty.send(DeviceEvent::Button {
+                    time: seconds(evt.time),
+                    code: ButtonCode::from_raw(evt.code),
+                    status: if evt.value == 1 { ButtonStatus::Pressed } else
+                                              { ButtonStatus::Released },
+                }).unwrap();
+            }
         }
     }
 }

src/view/common.rs
@@ -1,3 +1,4 @@
+use std::env;
 use view::{View, Event, Hub, ViewId, EntryId, EntryKind};
 use framebuffer::UpdateMode;
 use geom::{Point, Rectangle};
@@ -37,19 +38,29 @@ pub fn toggle_main_menu(view: &mut View, rect: Rectangle, enable: Option<bool>, 
         if let Some(false) = enable {
             return;
         }
-        let entries = &[EntryKind::CheckBox("Invert Colors".to_string(),
-                                            EntryId::ToggleInverted,
-                                            context.inverted),
-                        EntryKind::CheckBox("Make Bitonal".to_string(),
-                                            EntryId::ToggleMonochrome,
-                                            context.monochrome),
-                        EntryKind::Separator,
-                        EntryKind::Command("Take Screenshot".to_string(),
-                                           EntryId::TakeScreenshot),
-                        EntryKind::Separator,
-                        EntryKind::Command("Quit".to_string(),
-                                           EntryId::Quit)];
-        let main_menu = Menu::new(rect, ViewId::MainMenu, entries, fonts);
+        let mut entries = vec![EntryKind::CheckBox("Invert Colors".to_string(),
+                                                   EntryId::ToggleInverted,
+                                                   context.inverted),
+                               EntryKind::CheckBox("Make Bitonal".to_string(),
+                                                   EntryId::ToggleMonochrome,
+                                                   context.monochrome),
+                               EntryKind::Separator,
+                               EntryKind::Command("Take Screenshot".to_string(),
+                                                  EntryId::TakeScreenshot),
+                               EntryKind::Separator];
+        if env::var("ksmroot").is_ok() {
+            entries.push(EntryKind::Command("Quit".to_string(), EntryId::Quit));
+        } else {
+            entries.extend_from_slice(&[EntryKind::Command("Suspend".to_string(),
+                                                           EntryId::Suspend),
+                                        EntryKind::Command("Power Off".to_string(),
+                                                           EntryId::PowerOff),
+                                        EntryKind::Command("Reboot".to_string(),
+                                                           EntryId::Reboot),
+                                        EntryKind::Command("Start Nickel".to_string(),
+                                                           EntryId::StartNickel)]);
+        }
+        let main_menu = Menu::new(rect, ViewId::MainMenu, &entries, fonts);
         hub.send(Event::Render(*main_menu.rect(), UpdateMode::Gui)).unwrap();
         view.children_mut().push(Box::new(main_menu) as Box<View>);
     }

src/view/icon.rs
@@ -5,7 +5,7 @@ use framebuffer::{Framebuffer, Pixmap, UpdateMode};
 use view::{View, Event, Hub, Bus, Align};
 use view::BORDER_RADIUS_SMALL;
 use gesture::GestureEvent;
-use input::FingerStatus;
+use input::{DeviceEvent, FingerStatus};
 use document::pdf::PdfOpener;
 use unit::{scale_by_dpi, scale_by_dpi_raw};
 use font::Fonts;
@@ -61,7 +61,7 @@ impl Icon {
 impl View for Icon {
     fn handle_event(&mut self, evt: &Event, hub: &Hub, bus: &mut Bus, _context: &mut Context) -> bool {
         match *evt {
-            Event::Gesture(GestureEvent::Finger { status, ref position, .. }) => {
+            Event::Device(DeviceEvent::Finger { status, ref position, .. }) => {
                 match status {
                     FingerStatus::Down if self.rect.includes(position) => {
                         self.active = true;

src/view/key.rs
@@ -1,7 +1,7 @@
 use device::CURRENT_DEVICE;
 use framebuffer::{Framebuffer, UpdateMode};
+use input::{DeviceEvent, FingerStatus};
 use gesture::GestureEvent;
-use input::FingerStatus;
 use view::{View, Event, KeyboardEvent, Hub, Bus, TextKind};
 use view::BORDER_RADIUS_LARGE;
 use view::icon::ICONS_PIXMAPS;
@@ -88,7 +88,7 @@ impl Key {
 impl View for Key {
     fn handle_event(&mut self, evt: &Event, hub: &Hub, bus: &mut Bus, _context: &mut Context) -> bool {
         match *evt {
-            Event::Gesture(GestureEvent::Finger { status, ref position, .. }) => {
+            Event::Device(DeviceEvent::Finger { status, ref position, .. }) => {
                 match status {
                     FingerStatus::Down if self.rect.includes(position) => {
                         self.active = true;

src/view/menu_entry.rs
@@ -4,7 +4,7 @@ use framebuffer::{Framebuffer, UpdateMode};
 use geom::{Rectangle, CornerSpec, Dir};
 use view::{View, Event, Hub, Bus, EntryKind, BORDER_RADIUS_MEDIUM, THICKNESS_LARGE};
 use view::icon::ICONS_PIXMAPS;
-use input::FingerStatus;
+use input::{DeviceEvent, FingerStatus};
 use gesture::GestureEvent;
 use font::{Fonts, font_from_style, NORMAL_STYLE};
 use unit::scale_by_dpi;
@@ -34,7 +34,7 @@ impl MenuEntry {
 impl View for MenuEntry {
     fn handle_event(&mut self, evt: &Event, hub: &Hub, bus: &mut Bus, _context: &mut Context) -> bool {
         match *evt {
-            Event::Gesture(GestureEvent::Finger { status, ref position, .. }) => {
+            Event::Device(DeviceEvent::Finger { status, ref position, .. }) => {
                 match status {
                     FingerStatus::Down if self.rect.includes(position) => {
                         self.active = true;

commit 0ed2a210a1fff86e505efc925d7bd83e8bf08244

Fix regression introduced by feadc26

src/view/reader/mod.rs
@@ -113,7 +113,6 @@ impl Reader {
         })
     }
 
-
     fn go_to_page(&mut self, index: usize, hub: &Hub) {
         if index >= self.pages_count {
             return;
@@ -449,7 +448,10 @@ impl View for Reader {
                 self.toggle_margin_cropper(false, hub, &mut context.fonts);
                 true
             },
-            Event::Select(EntryId::Reboot) | Event::Select(EntryId::StartNickel) | Event::Back => {
+            Event::Select(EntryId::Quit) |
+            Event::Select(EntryId::Reboot) |
+            Event::Select(EntryId::StartNickel) |
+            Event::Back => {
                 self.quit(context);
                 false
             },