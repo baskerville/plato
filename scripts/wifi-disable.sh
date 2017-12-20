56a1233ccf9eb40f9b4852a0f0769cc5274dddf2
Bastien Dejean <nihilhill@gmail.com>
2017-12-20 14:32:16 +0100

    Reuse the environment variables defined in rcS

--- scripts/wifi-disable.sh
+++ scripts/wifi-disable.sh
@@ -2,8 +2,8 @@
 
 lsmod | grep -q sdio_wifi_pwr || exit 1
 
-wlarm_le -i eth0 down
-ifconfig eth0 down
+wlarm_le -i $INTERFACE down
+ifconfig $INTERFACE down
 
 rmmod -r "$WIFI_MODULE"
 rmmod -r sdio_wifi_pwr
--- scripts/wifi-enable.sh
+++ scripts/wifi-enable.sh
@@ -3,15 +3,15 @@
 lsmod | grep -q sdio_wifi_pwr && exit 1
 
 insmod /drivers/${PLATFORM}/wifi/sdio_wifi_pwr.ko
-insmod /drivers/${PLATFORM}/wifi/${WIFI_MODULE}.ko
+insmod "$WIFI_MODULE_PATH"
 
 sleep 1
 
-ifconfig eth0 up
-wlarm_le -i eth0 up
+ifconfig $INTERFACE up
+wlarm_le -i $INTERFACE up
 
-pidof wpa_supplicant > /dev/null || wpa_supplicant -D wext -s -i eth0 -c /etc/wpa_supplicant/wpa_supplicant.conf -C /var/run/wpa_supplicant -B
+pidof wpa_supplicant > /dev/null || wpa_supplicant -D wext -s -i $INTERFACE -c /etc/wpa_supplicant/wpa_supplicant.conf -C /var/run/wpa_supplicant -B
 
 sleep 1
 
-udhcpc -S -i eth0 -s /etc/udhcpc.d/default.script -t15 -T10 -A3 -b -q > /dev/null 2>&1 &
+udhcpc -S -i $INTERFACE -s /etc/udhcpc.d/default.script -t15 -T10 -A3 -b -q > /dev/null 2>&1 &
