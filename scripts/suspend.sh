ea892952a8d14ba65002045c712ace5b5d2c4989
Bastien Dejean <nihilhill@gmail.com>
2017-12-31 11:17:39 +0100

    Make suspend.sh work by adding a long delay

--- scripts/suspend.sh
+++ scripts/suspend.sh
@@ -1,7 +1,18 @@
 #! /bin/sh
 
+# Prevent false sleep state.
+# https://github.com/koreader/koreader/commit/71afe3606ca777e4e01fcb3c9a5323cf08bdfc0c
+sleep 15
+
+# De-activate the touch screen.
+echo 1 > /sys/power/state-extended
+
+# Prevent the following error on the last line:
+# *write error: Operation not permitted*.
+sleep 2
+
+# Synchronize the file system.
 sync
 
-echo 1 > /sys/power/state-extended
-[ "$PRODUCT" == phoenix ] && sleep 2
+# Suspend to RAM.
 echo mem > /sys/power/state
