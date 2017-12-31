b8c8b87c02cda316f12d887d36d83ba7bd45b397
Bastien Dejean <nihilhill@gmail.com>
2017-12-31 11:21:08 +0100

    Make wifi-disable be the opposite of wifi-enable

--- scripts/wifi-disable.sh
+++ scripts/wifi-disable.sh
@@ -2,10 +2,10 @@
 
 lsmod | grep -q sdio_wifi_pwr || exit 1
 
+killall udhcpc default.script wpa_supplicant 2> /dev/null
+
 wlarm_le -i $INTERFACE down
 ifconfig $INTERFACE down
 
 rmmod -r "$WIFI_MODULE"
 rmmod -r sdio_wifi_pwr
-
-killall udhcpc default.script wpa_supplicant dhcpcd-dbus dhcpcd 2> /dev/null
