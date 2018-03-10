baskerville/plato

commit 115c58f12d5f0517065ceb94448aa44c86ed3baa

Handle URIs of the form #P,A,B

src/view/reader/mod.rs
@@ -946,7 +946,7 @@ impl View for Reader {
                                      y_max as i32 - self.frame.min.y + dy];
 
                     if rect.includes(center) {
-                        let re = Regex::new(r"^([#@])(\d+)$").unwrap();
+                        let re = Regex::new(r"^([#@])(\d+)(?:,\d+,\d+)$").unwrap();
                         if let Some(caps) = re.captures(&link.uri) {
                             if let Ok(index) = caps[2].parse::<usize>() {
                                 if &caps[1] == "@" {

commit e3b8b041a33afdfc075a59f70fd0e6c1957825cb

Fix regression introduced by 115c58f

src/view/reader/mod.rs
@@ -946,7 +946,7 @@ impl View for Reader {
                                      y_max as i32 - self.frame.min.y + dy];
 
                     if rect.includes(center) {
-                        let re = Regex::new(r"^([#@])(\d+)(?:,\d+,\d+)$").unwrap();
+                        let re = Regex::new(r"^([#@])(\d+)(?:,\d+,\d+)?$").unwrap();
                         if let Some(caps) = re.captures(&link.uri) {
                             if let Ok(index) = caps[2].parse::<usize>() {
                                 if &caps[1] == "@" {