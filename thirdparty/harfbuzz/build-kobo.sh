402437217f772ef484e680186334b718c0a0ed82
serdimic <serdimic@eng.it>
2018-03-21 15:09:28 +0100

    bugfix on thirdparty build

--- thirdparty/harfbuzz/build-kobo.sh
+++ thirdparty/harfbuzz/build-kobo.sh
@@ -9,5 +9,4 @@ export CFLAGS="-O2 -mcpu=cortex-a9 -mfpu=neon"
 export CXXFLAGS="$CFLAGS"
 export FREETYPE_CFLAGS="-I${FREETYPE_DIR}/include"
 export FREETYPE_LIBS="-L${FREETYPE_DIR}/objs/.libs -L${LIBPNG_DIR}/.libs -L${BZIP2_DIR} -L${ZLIB_DIR} -lfreetype -lpng -lbz2 -lz"
-
-./autogen.sh --host=${TRIPLE} --disable-static --with-freetype=yes --with-fontconfig=no && make
+./autogen.sh --host=${TRIPLE} --disable-static --with-icu=no --with-freetype=yes --with-fontconfig=no && make
--- thirdparty/make-thirdparty.sh
+++ thirdparty/make-thirdparty.sh
@@ -1,29 +1,30 @@
 cd bzip2
-build-kobo.sh
+./build-kobo.sh
 ln -s libbz2.so.1.0 libbz2.so
 cd ..
 cd djvulibre
-build-kobo.sh
+./build-kobo.sh
 cd ..
 cd zlib
-build-kobo.sh
+./build-kobo.sh
 cd ..
 cd libpng
-build-kobo.sh
-ln -s .libs/libpng16.so .libs/libpng.so
-cd ..
+./build-kobo.sh
+cd .libs
+ln -s libpng16.so libpng.so
+cd ../..
 cd libjpeg
-build-kobo.sh
+./build-kobo.sh
 cd ..
 cd jbig2dec
-build-kobo.sh
+./build-kobo.sh
 cd ..
 cd freetype2
-build-kobo.sh no
+./build-kobo.sh no
 cd ..
 cd harfbuzz
-build-kobo.sh
+./build-kobo.sh
 cd ..
 cd freetype2
-build-kobo.sh
+./build-kobo.sh
 cd ..
