ca85ce3882e4acf5c36dd749de469b4ae021c14b
Bastien Dejean <nihilhill@gmail.com>
2018-10-04 21:59:16 +0200

    Disable WiFi: sleep a little before unloading
    On the Glo HD, without the additional sleep, the following command:
        rmmod -r "$WIFI_MODULE"
    might take about 40 seconds to complete!

--- scripts/wifi-disable.sh
+++ scripts/wifi-disable.sh
@@ -7,5 +7,6 @@ killall udhcpc default.script wpa_supplicant 2> /dev/null
 [ "$WIFI_MODULE" != 8189fs ] && wlarm_le -i "$INTERFACE" down
 ifconfig "$INTERFACE" down
 
+sleep 0.2
 rmmod -r "$WIFI_MODULE"
 rmmod -r sdio_wifi_pwr
