55f968a8954a3f26a8875e934a791d3e459b0839
Bastien Dejean <nihilhill@gmail.com>
2018-10-10 21:35:36 +0200

    Handle recent CPUs in scripts/usb-enable.sh

--- scripts/usb-enable.sh
+++ scripts/usb-enable.sh
@@ -21,15 +21,25 @@ VENDOR_ID=0x2237
 PRODUCT_ID=${PRODUCT_ID:-"0x6666"}
 FIRMWARE_VERSION=${FIRMWARE_VERSION:-"9.8.76543"}
 SERIAL_NUMBER=${SERIAL_NUMBER:-"N666999666999"}
-MODULE_PARAMETERS="vendor=${VENDOR_ID} product=${PRODUCT_ID} vendor_id=Kobo product_id=eReader-${FIRMWARE_VERSION} SN=${SERIAL_NUMBER}"
 
 GADGETS=/drivers/${PLATFORM}/usb/gadget
 
-if [ -e "$GADGETS"/arcotg_udc.ko ] ; then
-	# shellcheck disable=SC2086
-	insmod "$GADGETS"/arcotg_udc.ko $MODULE_PARAMETERS
-	sleep 2
-fi
+case "$PLATFORM" in
+	mx6[su]ll-ntx)
+		MODULE_PARAMETERS="idVendor=${VENDOR_ID} idProduct=${PRODUCT_ID} iManufacturer=Kobo iProduct=eReader-${FIRMWARE_VERSION} iSerialNumber=${SERIAL_NUMBER}"
+		insmod "$GADGETS"/configfs.ko
+		insmod "$GADGETS"/libcomposite.ko
+		insmod "$GADGETS"/usb_f_mass_storage.ko
+		;;
+	*)
+		MODULE_PARAMETERS="vendor=${VENDOR_ID} product=${PRODUCT_ID} vendor_id=Kobo product_id=eReader-${FIRMWARE_VERSION} SN=${SERIAL_NUMBER}"
+		if [ -e "$GADGETS"/arcotg_udc.ko ] ; then
+			# shellcheck disable=SC2086
+			insmod "$GADGETS"/arcotg_udc.ko $MODULE_PARAMETERS
+			sleep 2
+		fi
+		;;
+esac
 
 # shellcheck disable=SC2086
 insmod "$GADGETS"/g_file_storage.ko file="$PARTITIONS" stall=1 removable=1 $MODULE_PARAMETERS
