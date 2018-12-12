790a8d2ac85ea26e27c51c67895651885087bcee
Bastien Dejean <nihilhill@gmail.com>
2018-12-12 12:42:32 +0100

    Don't needlessly strip libmupdf.so

--- thirdparty/mupdf/build-kobo.sh
+++ thirdparty/mupdf/build-kobo.sh
@@ -5,4 +5,4 @@
 make verbose=yes generate
 make verbose=yes OS=kobo
 
-arm-linux-gnueabihf-gcc -Wl,--gc-sections -Wl,-s -o build/release/libmupdf.so $(find build/release -name '*.o' | grep -Ev '(/scripts/|SourceHanSans|DroidSansFallbackFull|color-lcms)') -lm -L../freetype2/objs/.libs -lfreetype -L../harfbuzz/src/.libs -lharfbuzz -L../jbig2dec/.libs -ljbig2dec -L../libjpeg/.libs -ljpeg -L../openjpeg/build/bin -lopenjp2 -L../zlib -lz -shared -Wl,-soname -Wl,libmupdf.so -Wl,--no-undefined
+arm-linux-gnueabihf-gcc -Wl,--gc-sections -o build/release/libmupdf.so $(find build/release -name '*.o' | grep -Ev '(/scripts/|SourceHanSans|DroidSansFallbackFull|color-lcms)') -lm -L../freetype2/objs/.libs -lfreetype -L../harfbuzz/src/.libs -lharfbuzz -L../jbig2dec/.libs -ljbig2dec -L../libjpeg/.libs -ljpeg -L../openjpeg/build/bin -lopenjp2 -L../zlib -lz -shared -Wl,-soname -Wl,libmupdf.so -Wl,--no-undefined
