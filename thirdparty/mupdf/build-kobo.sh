48d0bbb4949f60bb4a7e85e020892ea7019bbc7c
Bastien Dejean <nihilhill@gmail.com>
2018-12-13 21:35:14 +0100

    Handle optional BUILD_KIND argument

--- thirdparty/mupdf/build-kobo.sh
+++ thirdparty/mupdf/build-kobo.sh
@@ -2,7 +2,8 @@
 
 [ -e thirdparty/README ] && rm -rf thirdparty/*
 
+BUILD_KIND=${1:-release}
 make verbose=yes generate
-make verbose=yes OS=kobo
+make verbose=yes OS=kobo "$BUILD_KIND"
 
-arm-linux-gnueabihf-gcc -Wl,--gc-sections -o build/release/libmupdf.so $(find build/release -name '*.o' | grep -Ev '(/scripts/|SourceHanSans|DroidSansFallbackFull|color-lcms)') -lm -L../freetype2/objs/.libs -lfreetype -L../harfbuzz/src/.libs -lharfbuzz -L../jbig2dec/.libs -ljbig2dec -L../libjpeg/.libs -ljpeg -L../openjpeg/build/bin -lopenjp2 -L../zlib -lz -shared -Wl,-soname -Wl,libmupdf.so -Wl,--no-undefined
+arm-linux-gnueabihf-gcc -Wl,--gc-sections -o build/"$BUILD_KIND"/libmupdf.so $(find build/"$BUILD_KIND" -name '*.o' | grep -Ev '(/scripts/|SourceHanSans|DroidSansFallbackFull|color-lcms)') -lm -L../freetype2/objs/.libs -lfreetype -L../harfbuzz/src/.libs -lharfbuzz -L../jbig2dec/.libs -ljbig2dec -L../libjpeg/.libs -ljpeg -L../openjpeg/build/bin -lopenjp2 -L../zlib -lz -shared -Wl,-soname -Wl,libmupdf.so -Wl,--no-undefined
