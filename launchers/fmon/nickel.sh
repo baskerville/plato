3ddcbd35028c7ebd9581623d5ec9c1c34e8d21d3
NiLuJe <ninuje@gmail.com>
2018-12-28 22:20:21 +0100

    Unbreak nickel startup script
    Have to cd after running wifi-disable, since we're not using its
    absolute path ;).

--- launchers/fmon/nickel.sh
+++ launchers/fmon/nickel.sh
@@ -1,13 +1,15 @@
 #! /bin/sh
 
-cd /
-unset OLDPWD LC_ALL
 export LD_LIBRARY_PATH=/usr/local/Kobo
 
 ( usleep 400000; /etc/init.d/on-animator.sh ) &
 
 # Nickel wants the WiFi to be down when it starts
 ./scripts/wifi-disable.sh
+
+# Reset PWD to a sane value, outside of onboard, so that USBMS behaves properly
+cd /
+unset OLDPWD LC_ALL
 
 /usr/local/Kobo/hindenburg &
 /usr/local/Kobo/nickel -platform kobo -skipFontLoad &
