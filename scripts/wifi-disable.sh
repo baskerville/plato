9ab0830a9314655efb34dc4fdc0bdafc14f15b89
NiLuJe <ninuje@gmail.com>
2018-12-28 22:18:35 +0100

    Update WiFi scripts to handle yet another wifi special-case

--- scripts/wifi-disable.sh
+++ scripts/wifi-disable.sh
@@ -4,7 +4,7 @@ lsmod | grep -q sdio_wifi_pwr || exit 1
 
 killall udhcpc default.script wpa_supplicant 2> /dev/null
 
-[ "$WIFI_MODULE" != 8189fs ] && wlarm_le -i "$INTERFACE" down
+[ "$WIFI_MODULE" != 8189fs ] && [ "$WIFI_MODULE" != 8192es ] && wlarm_le -i "$INTERFACE" down
 ifconfig "$INTERFACE" down
 
 sleep 0.2
