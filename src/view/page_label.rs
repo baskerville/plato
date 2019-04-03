3ea3e7d640fe0bc7b8a65d2088a6ae28ad524d45
Bastien Dejean <nihilhill@gmail.com>
2019-04-03 11:25:14 +0200

    Adapt the page label format to the available width
    In addition, the format for synthetic pages now includes the byte stream
    position as a percentage.

--- src/view/page_label.rs
+++ src/view/page_label.rs
@@ -33,16 +33,29 @@ impl PageLabel {
         hub.send(Event::Render(self.rect, UpdateMode::Gui)).unwrap();
     }
 
-    pub fn text(&self) -> String {
+    pub fn text(&self, size: u8) -> String {
         if self.synthetic {
             let current_page = self.current_page as f64 / BYTES_PER_PAGE;
             let pages_count = self.pages_count as f64 / BYTES_PER_PAGE;
-            format!("Page {:.1} of {:.1}", current_page, pages_count)
+            let percent = 100.0 * current_page / pages_count;
+            match size {
+                0 => format!("Page {:.1} of {:.1} ({:.1}%)", current_page, pages_count, percent),
+                1 => format!("P. {:.1} of {:.1} ({:.1}%)", current_page, pages_count, percent),
+                2 => format!("{:.1}/{:.1} ({:.1}%)", current_page, pages_count, percent),
+                3 => format!("{:.1} ({:.1}%)", current_page, percent),
+                _ => format!("{:.1}%", percent),
+            }
         } else {
             if self.pages_count == 0 {
                 "No pages".to_string()
             } else {
-                format!("Page {} of {}", self.current_page + 1, self.pages_count)
+                match size {
+                    0 => format!("Page {} of {}", self.current_page + 1, self.pages_count),
+                    1 => format!("P. {} of {}", self.current_page + 1, self.pages_count),
+                    2 => format!("Page {}/{}", self.current_page + 1, self.pages_count),
+                    3 => format!("P. {}/{}", self.current_page + 1, self.pages_count),
+                    _ => format!("{}/{}", self.current_page + 1, self.pages_count),
+                }
             }
         }
     }
@@ -69,7 +82,14 @@ impl View for PageLabel {
         let font = font_from_style(fonts, &NORMAL_STYLE, dpi);
         let padding = font.em() as i32 / 2;
         let max_width = self.rect.width().saturating_sub(2 * padding as u32) as i32;
-        let plan = font.plan(&self.text(), None, None);
+        let mut plan = font.plan(&self.text(0), None, None);
+        for size in 1..=4 {
+            if plan.width <= max_width as u32 {
+                break;
+            }
+            plan = font.plan(&self.text(size), None, None);
+        }
+        font.crop_right(&mut plan, max_width as u32);
         let dx = padding + (max_width - plan.width as i32) / 2;
         let dy = (self.rect.height() as i32 - font.x_heights.0 as i32) / 2;
         let pt = pt!(self.rect.min.x + dx, self.rect.max.y - dy);
