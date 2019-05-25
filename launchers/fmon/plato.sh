73b42e6055f8d561c0d2ced14224e3a347368af8
Bastien Dejean <nihilhill@gmail.com>
2019-05-25 09:51:44 +0200

    fmon launcher: Enable backtraces by default

--- launchers/fmon/plato.sh
+++ launchers/fmon/plato.sh
@@ -12,6 +12,6 @@ export LD_LIBRARY_PATH="libs:${LD_LIBRARY_PATH}"
 
 [ -e info.log ] && [ $(stat -c '%s' info.log) -gt $((1<<18)) ] && mv info.log archive.log
 
-./plato >> info.log 2>&1 || mv info.log crash.log
+RUST_BACKTRACE=1 ./plato >> info.log 2>&1 || mv info.log crash.log
 
 ./nickel.sh &
