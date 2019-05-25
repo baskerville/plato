b6183ad56b41f3793a8d3bd340d18c5b3869478c
Bastien Dejean <nihilhill@gmail.com>
2019-05-25 09:55:07 +0200

    Fix a few shellcheck warnings

--- launchers/fmon/plato.sh
+++ launchers/fmon/plato.sh
@@ -3,14 +3,15 @@
 WORKDIR=$(dirname "$0")
 cd "$WORKDIR" || exit 1
 
-eval "$(xargs -n 1 -0 < /proc/$(pidof nickel)/environ | grep -E 'INTERFACE|WIFI_MODULE|DBUS_SESSION|NICKEL_HOME|LANG' | sed -e 's/^/export /')"
+eval "$(xargs -n 1 -0 < /proc/"$(pidof nickel)"/environ | grep -E 'INTERFACE|WIFI_MODULE|DBUS_SESSION|NICKEL_HOME|LANG' | sed -e 's/^/export /')"
 sync
 killall -TERM nickel hindenburg sickel fickel fmon > /dev/null 2>&1
 
-export MODEL_NUMBER=$(cut -f 6 -d ',' /mnt/onboard/.kobo/version | sed -e 's/^[0-]*//')
+MODEL_NUMBER=$(cut -f 6 -d ',' /mnt/onboard/.kobo/version | sed -e 's/^[0-]*//')
+export MODEL_NUMBER
 export LD_LIBRARY_PATH="libs:${LD_LIBRARY_PATH}"
 
-[ -e info.log ] && [ $(stat -c '%s' info.log) -gt $((1<<18)) ] && mv info.log archive.log
+[ -e info.log ] && [ "$(stat -c '%s' info.log)" -gt $((1<<18)) ] && mv info.log archive.log
 
 RUST_BACKTRACE=1 ./plato >> info.log 2>&1 || mv info.log crash.log
 
