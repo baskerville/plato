cec141d9aa0fb29939a93e21b6240b40e41df7a1
Bastien Dejean <nihilhill@gmail.com>
2019-07-16 16:49:22 +0200

    build.rs: Only emit outputs when cross-compiling

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
-    println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
+    if env::var("HOST") != env::var("TARGET") {
+        println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
+        println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
+    }
 }
