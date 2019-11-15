7ab5963a895cbabc2941de5cd8c768dec3ed67f6
Wayne Cheng <waynethecheng@gmail.com>
2019-11-15 10:39:54 -0500

    Link stdc++ to fix build failure from libdjvulibre

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 fn main() {
     if env::var("HOST") != env::var("TARGET") {
-        println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
+        println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m -l dylib=stdc++");
         println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
     }
 }
