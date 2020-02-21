baskerville/plato

commit e8809bd8ee83c62e8836c948283d8d1ed151f40a

Restore the proper rotation when opening fails

src/emulator.rs
@@ -349,6 +349,13 @@ pub fn run() -> Result<(), Error> {
                         history.push(view as Box<dyn View>);
                         view = next_view;
                     } else {
+                        if context.display.rotation != rotation {
+                            if let Ok(dims) = context.fb.set_rotation(rotation) {
+                                raw_sender.send(display_rotate_event(rotation)).ok();
+                                context.display.rotation = rotation;
+                                context.display.dims = dims;
+                            }
+                        }
                         handle_event(view.as_mut(), &Event::Invalid(info2), &tx, &mut bus, &mut context);
                     }
                 },

src/app.rs
@@ -792,6 +792,13 @@ pub fn run() -> Result<(), Error> {
                     });
                     view = next_view;
                 } else {
+                    if context.display.rotation != rotation {
+                        if let Ok(dims) = context.fb.set_rotation(rotation) {
+                            raw_sender.send(display_rotate_event(rotation)).ok();
+                            context.display.rotation = rotation;
+                            context.display.dims = dims;
+                        }
+                    }
                     handle_event(view.as_mut(), &Event::Invalid(info2), &tx, &mut bus, &mut context);
                 }
             },

commit 2c246e4a2241f68ded9d16b3ffb35532584edc3b

Fix regression introduced by e8809bd

src/emulator.rs
@@ -351,7 +351,6 @@ pub fn run() -> Result<(), Error> {
                     } else {
                         if context.display.rotation != rotation {
                             if let Ok(dims) = context.fb.set_rotation(rotation) {
-                                raw_sender.send(display_rotate_event(rotation)).ok();
                                 context.display.rotation = rotation;
                                 context.display.dims = dims;
                             }