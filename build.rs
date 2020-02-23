3d061e6ac755bca93b2ea1498bf160fd47721d3a
Thomas Hume <aszarsha@gmail.com>
2020-02-23 15:21:49 +0100

    fixes undefined references to libstdc++ at ld step

--- build.rs
+++ build.rs
@@ -4,5 +4,6 @@ fn main() {
     if env::var("HOST") != env::var("TARGET") {
         println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
         println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
+        println!("cargo:rustc-link-lib=dylib=stdc++");
     }
 }
