80153d8dc901b33286a6360944a8cee13c9f587e
Thomas Hume <aszarsha@gmail.com>
2020-03-03 00:20:12 +0100

    do not assume that the only host target is Linux

--- build.rs
+++ build.rs
@@ -1,20 +1,37 @@
 use std::env;
 
 fn main() {
-    if env::var("HOST") != env::var("TARGET") {   // assume cross-compiling for Kobo
+    let quadruple = env::var("TARGET").unwrap();
+    if quadruple == "arm-unknown-linux-gnueabihf" { //=> assume cross-compiling for Kobo
         println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
+
         println!("cargo:rustc-link-search=src/wrapper/Kobo");
         println!("cargo:rustc-link-search=libs");
-    } else {                                      // assume compiling for Linux host
-        println!("cargo:rustc-link-search=src/wrapper/Linux");
-        println!("cargo:rustc-link-lib=lcms2");
-        println!("cargo:rustc-link-lib=mupdf-third");
+
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    } else {
+        let tokens: Vec<&str> = quadruple.split('-').collect();
+        // [0] is <arch><sub> | [1] is <vendor> | [2] is <sys> | [3] is <abi>
+        let sys = tokens[2];
+
+        if sys == "linux" {                         //=> assume compiling for Linux host
+            println!("cargo:rustc-link-search=src/wrapper/Linux");
+
+            println!("cargo:rustc-link-lib=lcms2");
+            println!("cargo:rustc-link-lib=mupdf-third");
+            println!("cargo:rustc-link-lib=dylib=stdc++");
+
+        } else {                                    //=> assume compiling for MacOS host
+            println!("cargo:rustc-link-lib=mupdf-third");
+            println!("cargo:rustc-link-lib=dylib=c++");
+        }
     }
+    //=> In any case...
     println!("cargo:rustc-link-lib=z");
     println!("cargo:rustc-link-lib=bz2");
     println!("cargo:rustc-link-lib=jpeg");
     println!("cargo:rustc-link-lib=png16");
     println!("cargo:rustc-link-lib=openjp2");
     println!("cargo:rustc-link-lib=jbig2dec");
-    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
