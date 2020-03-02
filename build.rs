8db83f2987367db47eab23a7c798dd7a42cc1821
Thomas Hume <aszarsha@gmail.com>
2020-03-02 13:09:01 +0100

    update build.rs for host compilation

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
 use std::env;
 
 fn main() {
-    if env::var("HOST") != env::var("TARGET") {
-        println!("cargo:rustc-flags=-L libs -l jpeg -l png16 -l openjp2 -l jbig2dec -l bz2 -l z -l m");
+    if env::var("HOST") != env::var("TARGET") {   // assume cross-compiling for Kobo
         println!("cargo:rustc-env=PKG_CONFIG_ALLOW_CROSS=1");
-        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-search=libs");
+    } else {                                      // assume compiling for host
+        println!("cargo:rustc-link-search=src/wrapper");
+        println!("cargo:rustc-link-lib=lcms2");
+        println!("cargo:rustc-link-lib=mupdf-third");
     }
+    println!("cargo:rustc-link-lib=z");
+    println!("cargo:rustc-link-lib=bz2");
+    println!("cargo:rustc-link-lib=jpeg");
+    println!("cargo:rustc-link-lib=png16");
+    println!("cargo:rustc-link-lib=openjp2");
+    println!("cargo:rustc-link-lib=jbig2dec");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
