f15eca5f925749c6aeb5cb80d9f5b25d34822dca
Bastien Dejean <nihilhill@gmail.com>
2020-06-03 16:18:26 +0200

    Add missing block element names
    This fixes a regression introduced in 82886cb73d1e7937496ef31576219713dd6bed3b.
    
    Without the added names, `wrap_lost_inlines` will stop before reaching
    the 'body' element.

--- src/document/html/dom.rs
+++ src/document/html/dom.rs
@@ -87,9 +87,9 @@ impl Node {
         match *self {
             Node::Element(ElementData { ref name, .. }) => {
                 match name.as_str() {
-                    "address" | "article" | "aside" | "blockquote" | "details" | "dialog" |
+                    "address" | "anonymous" | "article" | "aside" | "blockquote" | "body" | "details" | "dialog" |
                     "dd" | "div" | "dl" | "dt" | "fieldset" | "figcaption" | "figure" | "footer" |
-                    "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "header" | "hgroup" | "hr" |
+                    "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "header" | "hgroup" | "hr" | "html" |
                     "li" | "main" | "nav" | "ol" | "p" | "pre" | "section" | "table" | "ul" => true,
                     _ => false,
                 }
