baskerville/plato

commit c4a5e043527fa575586fd2b22cbc646e2bedc10c

Make accessing the emulator and importer easier

build.sh
@@ -10,7 +10,7 @@ case "$method" in
 	fast)
 		./download.sh 'libs/*'
 		cd libs
-		
+
 		ln -s libz.so.1 libz.so
 		ln -s libbz2.so.1.0 libbz2.so
 
@@ -24,15 +24,13 @@ case "$method" in
 
 		ln -s libdjvulibre.so.21 libdjvulibre.so
 		;;
+
 	slow)
 		shift
 		cd thirdparty
 		./download.sh "$@"
 		./build.sh "$@"
 		cd ..
-		cd src/wrapper
-		./build-kobo.sh
-		cd ../..
 
 		[ -e libs ] || mkdir libs
 
@@ -49,7 +47,6 @@ case "$method" in
 
 		cp thirdparty/djvulibre/libdjvu/.libs/libdjvulibre.so libs
 		cp thirdparty/mupdf/build/release/libmupdf.so libs
-		cp src/wrapper/libmupdfwrapper.so libs
 		;;
 
 	skip)
@@ -59,5 +56,9 @@ case "$method" in
 		exit 1
 		;;
 esac
+
+cd src/mupdf_wrapper
+./build-kobo.sh
+cd ../..
 
 cargo build --release --target=arm-unknown-linux-gnueabihf

src/mupdf_wrapper/build.sh
@@ -1,0 +1,11 @@
+#! /bin/sh
+
+set -e
+
+CC=${CC:-gcc}
+AR=${AR:-ar}
+
+TARGET_OS=${TARGET_OS:-$(uname -s)}
+mkdir -p ${TARGET_OS}
+$CC $CPPFLAGS $CFLAGS -c mupdf_wrapper.c -o ${TARGET_OS}/mupdf_wrapper.o
+$AR rcs ${TARGET_OS}/libmupdf_wrapper.a ${TARGET_OS}/mupdf_wrapper.o

src/wrapper/build.sh
@@ -1,15 +1,0 @@
-#! /bin/sh
-
-CC=${CC:-gcc}
-LD=${LD:-ld}
-
-TARGET_OS=${TARGET_OS:-$(uname -s)}
-if [ "$TARGET_OS" = "Darwin" ] ; then
-	LIB_EXT=dylib
-else
-	LIB_EXT=so
-fi
-
-$CC $CPPFLAGS $CFLAGS -fPIC -c mupdf.c
-$CC $LDFLAGS -shared -fPIC -o libmupdfwrapper.${LIB_EXT} mupdf.o
-rm mupdf.o

.github/workflows/armhf.yml
@@ -1,13 +1,0 @@
-name: armhf
-
-on: [pull_request]
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v1
-    - name: (armhf) build image
-      run: docker build . -f Dockerfile -t plato:armhf
-    - name: (armhf) build plato
-      run: docker run --rm plato:armhf

.github/workflows/dev.yml
@@ -1,13 +1,0 @@
-name: dev
-
-on: [pull_request]
-
-jobs:
-  build:
-    runs-on: ubuntu-latest
-    steps:
-    - uses: actions/checkout@v1
-    - name: (dev) build image
-      run: docker build . -f Dockerfile.dev -t plato:dev
-    - name: (dev) run tests and build all features
-      run: docker run --rm plato:dev

.gitignore
@@ -9,8 +9,7 @@
 /css/*-user.css
 /hyphenation-patterns
 /dictionaries
-/src/wrapper/*.so
-/src/wrapper/*.dylib
+/src/mupdf_wrapper/*/
 /dist
 /plato-*.zip
 /thirdparty/*/*

Dockerfile
@@ -1,14 +1,0 @@
-FROM rust:1.39-buster
-
-RUN /usr/bin/dpkg --add-architecture armhf
-RUN apt-get update && apt-get install -y pkg-config \
-	gcc-arm-linux-gnueabihf \
-	g++-arm-linux-gnueabihf \
-	jq
-
-RUN rustup target add arm-unknown-linux-gnueabihf
-
-# Build plato
-WORKDIR /plato
-ADD . /plato
-CMD ["./build.sh"]

Dockerfile.dev
@@ -1,29 +1,0 @@
-FROM rust:1.39-buster
-
-RUN apt-get update && apt-get install -y libtool \
-	pkg-config \
-	jq \
-	libdjvulibre-dev \
-	libharfbuzz-dev \
-	libsdl2-dev \
-	# start mupdf dependencies
-	libjbig2dec0-dev \
-	patch
-	# end mupdf dependencies
-
-WORKDIR /tmp
-
-# Build modified mupdf and wrapper
-COPY contrib/ /tmp/contrib/
-COPY thirdparty/ /tmp/thirdparty/
-COPY src/wrapper/ /tmp/wrapper/
-RUN wget -qO - "https://mupdf.com/downloads/archive/mupdf-1.16.0-source.tar.gz" | tar -xz --strip-components 1 -C "/tmp/thirdparty/mupdf/" \
-	&& cp /tmp/contrib/linux/mupdf/* /tmp/thirdparty/mupdf/ \
-	&& cd /tmp/thirdparty/mupdf/ && (patch -p1 < linux.patch) && ./build-linux.sh && cp /tmp/thirdparty/mupdf/build/release/libmupdf.so /usr/lib/ \
-	&& cd /tmp/wrapper/ && CFLAGS="-I /tmp/thirdparty/mupdf/include" ./build.sh && cp /tmp/wrapper/libmupdfwrapper.so /usr/lib/
-
-# Build plato
-WORKDIR /plato
-
-ADD . /plato
-CMD ["bash", "-c", "cargo test && cargo build --all-features"]

commit 7eeec30dc703630aa7ac9e8e4bae130bbe52eeab

We need the MuPDF headers to build the wrapper
`./build.sh fast` was broken by c4a5e043527fa575586fd2b22cbc646e2bedc10c.

build.sh
@@ -24,6 +24,8 @@ case "$method" in
 
 		ln -s libdjvulibre.so.21 libdjvulibre.so
 
+		cd ../thirdparty
+		./download.sh mupdf
 		cd ..
 		;;
 