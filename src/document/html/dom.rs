baskerville/plato

commit 82886cb73d1e7937496ef31576219713dd6bed3b

Define `is_block` directly

src/document/html/dom.rs
@@ -83,27 +83,18 @@ impl Node {
         }
     }
 
-    pub fn is_inline(&self) -> bool {
+    pub fn is_block(&self) -> bool {
         match *self {
-            Node::Text(..) => true,
             Node::Element(ElementData { ref name, .. }) => {
                 match name.as_str() {
-                    "span" | "em" | "strong" | "i" | "b" | "img" |
-                    "a" | "br" | "code" | "sub" | "sup" | "dfn" |
-                    "big" | "small" | "abbr" | "cite" | "var" |
-                    "del" | "ins" | "samp" | "kbd" | "q" |
-                    "image" | "svg:image" => true,
+                    "address" | "article" | "aside" | "blockquote" | "details" | "dialog" |
+                    "dd" | "div" | "dl" | "dt" | "fieldset" | "figcaption" | "figure" | "footer" |
+                    "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "header" | "hgroup" | "hr" |
+                    "li" | "main" | "nav" | "ol" | "p" | "pre" | "section" | "table" | "ul" => true,
                     _ => false,
                 }
             },
             _ => false,
-        }
-    }
-
-    pub fn is_block(&self) -> bool {
-        match *self {
-            Node::Whitespace(..) => false,
-            _ => !self.is_inline(),
         }
     }
 

commit 17c742048f19b59c5c94751246ed3bc3bf208c7b

Add missing block elements
Tables were broken since 82886cb73d1e7937496ef31576219713dd6bed3b.

src/document/html/dom.rs
@@ -97,10 +97,11 @@ impl Node {
         match *self {
             Node::Element(ElementData { ref name, .. }) => {
                 match name.as_str() {
-                    "address" | "anonymous" | "article" | "aside" | "blockquote" | "body" | "details" | "dialog" |
-                    "dd" | "div" | "dl" | "dt" | "fieldset" | "figcaption" | "figure" | "footer" |
-                    "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "header" | "hgroup" | "hr" | "html" |
-                    "li" | "main" | "nav" | "ol" | "p" | "pre" | "section" | "table" | "ul" => true,
+                    "address" | "anonymous" | "article" | "aside" | "blockquote" | "body" | "head" |
+                    "details" | "dialog" | "dd" | "div" | "dl" | "dt" | "fieldset" | "figcaption" |
+                    "figure" | "footer" | "form" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "header" |
+                    "hgroup" | "hr" | "html" | "li" | "main" | "nav" | "ol" | "p" | "pre" | "section" |
+                    "table" | "thead" | "tbody" | "tr" | "td" | "th" | "ul" => true,
                     _ => false,
                 }
             },