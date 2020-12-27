c6f0ca1dbe12674a735cdbc5a444d6368ab47894
Bastien Dejean <nihilhill@gmail.com>
2020-12-27 19:28:26 +0100

    Update thirdparty dependencies

--- thirdparty/download.sh
+++ thirdparty/download.sh
@@ -3,18 +3,18 @@
 declare -A urls=(
 	# Compression
 	["zlib"]="https://zlib.net/zlib-1.2.11.tar.gz"
-	["bzip2"]="https://ftp.osuosl.org/pub/clfs/conglomeration/bzip2/bzip2-1.0.6.tar.gz"
+	["bzip2"]="ftp://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz"
 	# Images
 	["libpng"]="https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz"
 	["libjpeg"]="http://www.ijg.org/files/jpegsrc.v9d.tar.gz"
 	["openjpeg"]="https://github.com/uclouvain/openjpeg/archive/v2.3.1.tar.gz"
-	["jbig2dec"]="https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs952/jbig2dec-0.18.tar.gz"
+	["jbig2dec"]="https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9533/jbig2dec-0.19.tar.gz"
 	# Fonts
-	["freetype2"]="https://download.savannah.gnu.org/releases/freetype/freetype-2.10.3.tar.gz"
-	["harfbuzz"]="https://github.com/harfbuzz/harfbuzz/archive/2.7.2.tar.gz"
+	["freetype2"]="https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz"
+	["harfbuzz"]="https://github.com/harfbuzz/harfbuzz/archive/2.7.4.tar.gz"
 	# Documents
 	["gumbo"]="https://github.com/google/gumbo-parser/archive/v0.10.1.tar.gz"
-	["djvulibre"]="http://downloads.sourceforge.net/djvu/djvulibre-3.5.27.tar.gz"
+	["djvulibre"]="http://downloads.sourceforge.net/djvu/djvulibre-3.5.28.tar.gz"
 	["mupdf"]="https://mupdf.com/downloads/archive/mupdf-1.18.0-source.tar.gz"
 )
 
