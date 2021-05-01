e231bfc63d574345f32285aa4bfbe22286278316
Bastien Dejean <nihilhill@gmail.com>
2021-05-01 11:48:32 +0200

    Convert StarDict dictionaries in `plato.sh`

---
+++ contrib/config-sample.sh
@@ -0,0 +1,8 @@
+# Don't set the framebuffer's depth.
+# unset PLATO_SET_FRAMEBUFFER_DEPTH
+
+# Don't convert StarDict dictionaries.
+# unset PLATO_CONVERT_DICTIONARIES
+
+# Disable hyphenation.
+# [ -d hyphenation-patterns ] && rm -rf hyphenation-patterns
