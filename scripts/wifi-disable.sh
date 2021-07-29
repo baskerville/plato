6cd612b1bf25fb8ad23056444a4d3f3cd1caa09c
Bastien Dejean <nihilhill@gmail.com>
2021-07-29 17:21:49 +0200

    Simplify WiFi module criterion

--- scripts/wifi-disable.sh
+++ scripts/wifi-disable.sh
@@ -4,7 +4,7 @@ lsmod | grep -q sdio_wifi_pwr || exit 1
 
 killall udhcpc default.script wpa_supplicant 2> /dev/null
 
-[ "$WIFI_MODULE" != 8189fs ] && [ "$WIFI_MODULE" != 8192es ] && [ "$WIFI_MODULE" != 8821cs ] && wlarm_le -i "$INTERFACE" down
+[ "$WIFI_MODULE" = dhd ] && wlarm_le -i "$INTERFACE" down
 ifconfig "$INTERFACE" down
 
 sleep 0.2
