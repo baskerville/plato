397e3958f9112f2421c22140cb5af314c54da60a
Bastien Dejean <nihilhill@gmail.com>
2022-06-26 22:14:51 +0200

    Update third-party dependencies

--- thirdparty/download.sh
+++ thirdparty/download.sh
@@ -7,11 +7,11 @@ declare -A urls=(
 	# Images
 	["libpng"]="https://download.sourceforge.net/libpng/libpng-1.6.37.tar.gz"
 	["libjpeg"]="http://www.ijg.org/files/jpegsrc.v9e.tar.gz"
-	["openjpeg"]="https://github.com/uclouvain/openjpeg/archive/v2.4.0.tar.gz"
+	["openjpeg"]="https://github.com/uclouvain/openjpeg/archive/v2.5.0.tar.gz"
 	["jbig2dec"]="https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9533/jbig2dec-0.19.tar.gz"
 	# Fonts
-	["freetype2"]="https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.gz"
-	["harfbuzz"]="https://github.com/harfbuzz/harfbuzz/archive/3.0.0.tar.gz"
+	["freetype2"]="https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.gz"
+	["harfbuzz"]="https://github.com/harfbuzz/harfbuzz/archive/3.0.0.tar.gz" # We're stuck at 3.0.0.
 	# Documents
 	["gumbo"]="https://github.com/google/gumbo-parser/archive/v0.10.1.tar.gz"
 	["djvulibre"]="http://downloads.sourceforge.net/djvu/djvulibre-3.5.28.tar.gz"
